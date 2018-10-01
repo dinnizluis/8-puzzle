e7e0f49936aec3ceef080a640b53b073a5868424
dinnizluis <dinnizluis@gmail.com>
2018-10-01 14:44:41 -0300

    Changes made just for tests

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from graph import *
 
 #''' Initial test of the board class
-file = open('in1', 'r')
+file = open('in2', 'r')
 board = Board()
-board.readFromFile(file)
-root = Node(board)
-graph = Graph(root)
+#board.readFromFile(file)
+#root = Node(board)
+graph = Graph(Node(board.readFromFile(file)))
 graph.buildGraph()
 children = graph.getRoot().getChildren()
-print(graph.root.children[1])
+graph.root.children[1].printNode()
+print(graph.root.children[1].getBoard().isFinal())
 #'''
+'''board1, board2 = Board(), Board()
+
+conf1, conf2 = [1,2,3,0,4,5,6,7,8], [1,2,3,4,0,5,6,7,8]
+
+board1.setConf(conf1)
+board2.setConf(conf2)
+
+print(board1.getConf())
+print(board2.getConf())
+
+print(board1.sameBoard(board2))'''
