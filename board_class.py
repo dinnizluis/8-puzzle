6eba1b90f6ca1859c7a2d431eee908103acfac7b
dinnizluis <dinnizluis@gmail.com>
2018-10-17 15:35:27 -0300

    Funcionou, A* aplicada

--- board_class.py
+++ board_class.py
@@ -1,4 +1,5 @@
 from anytree import *
+import time, timeit
 
 class Board(NodeMixin): #Add the node feature
 
@@ -10,120 +11,161 @@ class Board(NodeMixin): #Add the node feature
 			self.status = "not_final"
 		self.parent= parent
 
+	#Mostra toda a árvore de busca construída.
 	def printTree(self):
 		for pre, _, node in RenderTree(self):
 			treestr = u"%s%s" % (pre, node.conf)
 			print(treestr.ljust(8), node.status)
 
+	#Mostra tabuleiro atual.
 	def printBoard(self):
-		#print("{} is the configuration.".format(self.conf))
-		#print("{} is the status.".format(self.status))
-		print("configuration: ")
-		print(self.conf)
-		print("status: ")
-		print(self.status)
+		print("{} is the configuration.".format(self.conf))
+		print("{} is the status.".format(self.status))
 
+	#Cria uma nova referência e copia os dados do board.
 	def copyBoard(self):
 		board = Board(self.conf.copy())
 		return board
 
+	#Troca dois elementos nas posições a e b (um com o outro).
 	def swap(self, a, b):
 		lst = self.conf.copy()
 		lst[a], lst[b] = lst[b], lst[a]
 		return lst
 
+	#Retorna em qual posição o 0 (vazio) está.
 	def findZero(self):
 		for i in range(len(self.conf)):
 			if self.conf[i] == 0:
 				return i
 		return -1
 
+	#Retorna se o board é final.
 	def boardIsFinal(self):
 		return self.status == "final"
 
+	#Calcula o valor heurístico para o tabuleiro.
+	def heuristic_value(self):
+		h = 0
+		for i in self.conf:
+			h = h + self.manhattanDistance(self.conf[i], i)
+		return h
+
+	#Método capaz de calcular a distância de Manhattan de elem que está na posição pos.
+	#Retorna o valor absoluto da distância.
+	def manhattanDistance(self, elem, pos):
+		if pos>=0 and pos<=2:
+			cord = (0, pos)
+		elif pos>=3 and pos<=5:
+			cord = (1, pos-3)
+		elif pos>=5 and pos<=8:
+			cord = (2, pos-6)
+		else:
+			return -1 #Invalid position
+
+		if elem == 0:
+			return cord[0]+cord[1]
+		elif elem == 1:
+			return abs(cord[0]-0) + abs(cord[1]-1)
+		elif elem == 2:
+			return abs(cord[0]-0) + abs(cord[1]-2)
+		elif elem == 3:
+			return abs(cord[0]-1) + abs(cord[1]-0)
+		elif elem == 4:
+			return abs(cord[0]-1) + abs(cord[1]-1)
+		elif elem == 5:
+			return abs(cord[0]-1) + abs(cord[1]-2)
+		elif elem == 6:
+			return abs(cord[0]-2) + abs(cord[1]-0)
+		elif elem == 7:
+			return abs(cord[0]-2) + abs(cord[1]-1)
+		elif elem == 8:
+			return abs(cord[0]-2) + abs(cord[1]-2)
+
+	#Método capaz de realizar os movimentos do jogo. De acordo com a posição do quadro vazio os novos estados são gerados na ordem CBDE.
 	def haveChild(self):
 		zero = self.findZero()
 		if zero == 1:
-			left = self.swap(zero, zero+1)
+			up = self.swap(zero, zero+3)#UP
+			count = 0
+			for i in range(len(self.ancestors)):
+				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == up)) != 0:#Verifica se node já existe na árvore
+					count +=1
+			if count == 0:#Se não existir realiza movimento (up)
+				node_up = Board(up, parent=self)
+
+			left = self.swap(zero, zero-1)#LEFT
 			count = 0
 			for i in range(len(self.ancestors)):
 				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == left)) != 0:
-					count +=1 
+					count +=1
 			if count == 0:
 				node_left = Board(left, parent=self)
 
-			mid = self.swap(zero, zero+3)
-			count = 0
-			for i in range(len(self.ancestors)):
-				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == mid)) != 0:
-					count +=1 
-			if count == 0:
-				node_left = Board(mid, parent=self)
-
-			right = self.swap(zero, zero-1)
+			right = self.swap(zero, zero+1)#RIGHT
 			count = 0
 			for i in range(len(self.ancestors)):
 				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == right)) != 0:
-					count +=1 
+					count +=1
 			if count == 0:
-				node_left = Board(right, parent=self)
+				node_right = Board(right, parent=self)
 
 		elif zero == 3:
-			left = self.swap(zero, zero+1)
+			up = self.swap(zero, zero-3)#UP
+			count = 0
+			for i in range(len(self.ancestors)):
+				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == up)) != 0:
+					count +=1
+			if count == 0:
+				node_up = Board(up, parent=self)
+
+			down = self.swap(zero, zero+3)#DOWN
+			count = 0
+			for i in range(len(self.ancestors)):
+				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == down)) != 0:
+					count +=1
+			if count == 0:
+				node_down = Board(down, parent=self)
+
+			left = self.swap(zero, zero+1)#LEFT
 			count = 0
 			for i in range(len(self.ancestors)):
 				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == left)) != 0:
-					count +=1 
+					count +=1
 			if count == 0:
 				node_left = Board(left, parent=self)
-
-			mid = self.swap(zero, zero+3)
-			count = 0
-			for i in range(len(self.ancestors)):
-				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == mid)) != 0:
-					count +=1 
-			if count == 0:
-				node_left = Board(mid, parent=self)
-
-			right = self.swap(zero, zero-3)
-			count = 0
-			for i in range(len(self.ancestors)):
-				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == right)) != 0:
-					count +=1 
-			if count == 0:
-				node_left = Board(right, parent=self)
 
 		elif zero == 5:
-			left = self.swap(zero, zero-1)
+			up = self.swap(zero, zero-3)#UP
 			count = 0
 			for i in range(len(self.ancestors)):
-				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == left)) != 0:
-					count +=1 
+				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == up)) != 0:
+					count +=1
 			if count == 0:
-				node_left = Board(left, parent=self)
+				node_up = Board(up, parent=self)
 
-			mid = self.swap(zero, zero+3)
+			down = self.swap(zero, zero+3)#DOWN
 			count = 0
 			for i in range(len(self.ancestors)):
-				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == mid)) != 0:
-					count +=1 
+				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == down)) != 0:
+					count +=1
 			if count == 0:
-				node_left = Board(mid, parent=self)
+				node_down = Board(down, parent=self)
 
-			right = self.swap(zero, zero-3)
+			right = self.swap(zero, zero-1)#RIGHT
 			count = 0
 			for i in range(len(self.ancestors)):
 				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == right)) != 0:
-					count +=1 
+					count +=1
 			if count == 0:
-				node_left = Board(right, parent=self)
-		
+				node_right = Board(right, parent=self)
+
 		elif zero == 7:
 			left = self.swap(zero, zero-1)
 			count = 0
 			for i in range(len(self.ancestors)):
 				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == left)) != 0:
-					count +=1 
+					count +=1
 			if count == 0:
 				node_left = Board(left, parent=self)
 
@@ -131,45 +173,45 @@ class Board(NodeMixin): #Add the node feature
 			count = 0
 			for i in range(len(self.ancestors)):
 				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == mid)) != 0:
-					count +=1 
+					count +=1
 			if count == 0:
-				node_left = Board(mid, parent=self)
+				node_mid = Board(mid, parent=self)
 
 			right = self.swap(zero, zero-3)
 			count = 0
 			for i in range(len(self.ancestors)):
 				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == right)) != 0:
-					count +=1 
+					count +=1
 			if count == 0:
-				node_left = Board(right, parent=self)
+				node_right = Board(right, parent=self)
 
 		elif zero == 0:
-			left = self.swap(zero, zero+1)
+			up = self.swap(zero, zero+3)#UP
+			count = 0
+			for i in range(len(self.ancestors)):
+				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == up)) != 0:
+					count +=1
+			if count == 0:
+				node_up = Board(up, parent=self)
+
+			left = self.swap(zero, zero+1)#LEFT
 			count = 0
 			for i in range(len(self.ancestors)):
 				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == left)) != 0:
-					count +=1 
+					count +=1
 			if count == 0:
 				node_left = Board(left, parent=self)
-
-			right = self.swap(zero, zero+3)
-			count = 0
-			for i in range(len(self.ancestors)):
-				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == right)) != 0:
-					count +=1 
-			if count == 0:
-				node_left = Board(right, parent=self)
 
 		elif zero == 2:
-			left = self.swap(zero, zero-1)
+			up = self.swap(zero, zero+3)#UP
 			count = 0
 			for i in range(len(self.ancestors)):
-				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == left)) != 0:
-					count +=1 
+				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == up)) != 0:
+					count +=1
 			if count == 0:
-				node_left = Board(left, parent=self)
+				node_up = Board(up, parent=self)
 
-			right = self.swap(zero, zero+3)
+			right = self.swap(zero, zero-1)#RIGHT
 			count = 0
 			for i in range(len(self.ancestors)):
 				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == right)) != 0:
@@ -178,15 +220,15 @@ class Board(NodeMixin): #Add the node feature
 				node_right = Board(right, parent=self)
 
 		elif zero == 6:
-			left = self.swap(zero, zero+1)
+			down = self.swap(zero, zero-3)#DOWN
 			count = 0
 			for i in range(len(self.ancestors)):
-				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == left)) != 0:
-					count +=1 
+				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == down)) != 0:
+					count +=1
 			if count == 0:
-				node_left = Board(left, parent=self)
+				node_down = Board(down, parent=self)
 
-			right = self.swap(zero, zero-3)
+			right = self.swap(zero, zero+1)#RIGHT
 			count = 0
 			for i in range(len(self.ancestors)):
 				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == right)) != 0:
@@ -195,24 +237,40 @@ class Board(NodeMixin): #Add the node feature
 				node_right = Board(right, parent=self)
 
 		elif zero == 8:
-			left = self.swap(zero, zero-1)
+			down = self.swap(zero, zero-3)#DOWN
 			count = 0
 			for i in range(len(self.ancestors)):
-				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == left)) != 0:
-					count +=1 
-			if count == 0:
-				node_left = Board(left, parent=self)
-
-			right = self.swap(zero, zero-3)
-			count = 0
-			for i in range(len(self.ancestors)):
-				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == right)) != 0:
+				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == down)) != 0:
 					count +=1
 			if count == 0:
-				node_right = Board(right, parent=self)
+				node_down = Board(down, parent=self)
+
+			left = self.swap(zero, zero-1)#LEFT
+			count = 0
+			for i in range(len(self.ancestors)):
+				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == left)) != 0:
+					count +=1
+			if count == 0:
+				node_left = Board(left, parent=self)
 
 		elif zero == 4:
-			left = self.swap(zero, zero-1)
+			up = self.swap(zero, zero-3)#UP
+			count = 0
+			for i in range(len(self.ancestors)):
+				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == up)) != 0:
+					count += 1
+			if count == 0:
+				node_up = Board(up, parent=self)
+
+			down = self.swap(zero, zero+3)#DOWN
+			count = 0
+			for i in range(len(self.ancestors)):
+				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == down)) != 0:
+					count += 1
+			if count == 0:
+				node_down = Board(down, parent=self)
+
+			left = self.swap(zero, zero-1)#LEFT
 			count = 0
 			for i in range(len(self.ancestors)):
 				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == left)) != 0:
@@ -220,23 +278,7 @@ class Board(NodeMixin): #Add the node feature
 			if count == 0:
 				node_left = Board(left, parent=self)
 
-			mid1 = self.swap(zero, zero+1)
-			count = 0
-			for i in range(len(self.ancestors)):
-				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == mid1)) != 0:
-					count += 1
-			if count == 0:
-				node_mid1 = Board(mid1, parent=self)
-
-			mid2 = self.swap(zero, zero-3)
-			count = 0
-			for i in range(len(self.ancestors)):
-				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == mid2)) != 0:
-					count += 1
-			if count == 0:
-				node_mid2 = Board(mid2, parent=self)
-
-			right = self.swap(zero, zero+3)
+			right = self.swap(zero, zero+1)#RIGHT
 			count = 0
 			for i in range(len(self.ancestors)):
 				if len(findall(self.ancestors[i], filter_=lambda node: node.conf == right)) != 0:
@@ -246,29 +288,31 @@ class Board(NodeMixin): #Add the node feature
 
 		else:
 			return False
-
-
+	
+	#Método capaz de encontrar caminho mínimo do board no parâmetro até um estado final,
+	#retorna o custo mínimo e quantidade de nós analisados.
 	def findMinCost(self):
 		if self.boardIsFinal():
 			return 0
 		else:
 			q = []
-			q.append(self)
-			cost = 0
+			r = (self.heuristic_value(), self)
+			q.append(r)
+			nodes = 0
 			while len(q) != 0:
-				print(len(q))
-				#wait = input('The tree is currently like this: ')
-				#self.printTree()
-				#print('\n')
-				u = q.pop(0)
-				u.haveChild()
-				for i in range(len(u.children)):
-					if u.children[i].boardIsFinal():
-						return len(u.ancestors)+1
+				u = min(q, key=lambda q: q[0])
+				q.remove(u)
+				u[1].haveChild()
+				for i in range(len(u[1].children)):
+					nodes +=1
+					if u[1].children[i].boardIsFinal():
+						return len(u[1].ancestors)+1, nodes
 					else:
-						q.append(u.children[i])
+						g = len(u[1].ancestors)+1
+						h = u[1].children[i].heuristic_value()
+						q.append( (h+g, u[1].children[i]) )
 
-
+#Método capaz de ler um arquivo com a configuração de um tabuleiro e retorná-lo como uma lista.
 def readFromFile(name):
 	file = open(name, 'r')
 	for row in file:
@@ -279,6 +323,35 @@ def readFromFile(name):
 		conf[8] = temp[0]
 	return [int(i) for i in conf]
 
-board0 = Board(readFromFile('in3'))
-print("custo_total: {}".format(board0.findMinCost()))
-board0.printTree()
+#Método para executar os 9 exemplos disponíveis em uma única execução.
+def run(a, b):
+	file = open("results.txt", "w")
+	file.close()
+	for i in range(a, b+1):
+		board = Board(readFromFile('testes/in'+str(i)))
+		begin = time.time()
+		rslt = board.findMinCost()
+		end = time.time()
+		tempo = end - begin
+		file0 = open("results.txt", "a")
+		file1 = open("out"+str(i), "w")
+		file0.write("custo_total: {0}, nós examinados: {1}, tempo: {2} segundos".format(rslt[0], rslt[1], str(tempo)))
+		file0.write("\n")
+		file1.write("custo_total: {0}".format(rslt[0]))
+		file0.close()
+		file1.close()
+		print("------------------------------------------------------------------------------------")
+		print("In"+str(i))
+		print("custo_total: {0}, nós examinados: {1}, tempo: {2} segundos".format(rslt[0], rslt[1], str(tempo)))
+
+#lib para calcular quantidade de memória alocada.
+import tracemalloc
+tracemalloc.start()
+
+run(1,9)
+
+snapshot = tracemalloc.take_snapshot()
+top_stats = snapshot.statistics('lineno')
+print('[Top 10]')
+for stat in top_stats[:10]:
+	print(stat)
